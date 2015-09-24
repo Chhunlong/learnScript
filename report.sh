e311efa76c70036245afdd5a275193e364ac6f88
long <long@web-essentials.asia>
2015-09-24 12:38:52 +0700

    Chapter 2 : Learning script with input and output with echo and printf

--- report.sh
+++ report.sh
@@ -1,2 +1,12 @@
 #!/usr/bin/env bash
-printf "Hello world"
+#: Description      : 'print the formated for sale reported'
+divider=====================================================
+divider=$divider$divider
+
+## Format string for sale report
+header="\n %-10s %11s %8s %10s\n"
+
+
+## Print the header
+printf "$header" ITEM "PER UNIT" NUM TOTAL
+
